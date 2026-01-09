4daed3c8f1b1e7f5efd528f74061c38eba758c98
Tonkaw <tonkaw.zaa@gmail.com>
2026-01-09 19:47:47 +0700

    Mark AuthProvider props as readonly

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react"
 export default function AuthProvider({
   children,
 }: {
-  children: React.ReactNode
+  readonly children: React.ReactNode
 }) {
   return <SessionProvider>{children}</SessionProvider>
 }
